27e6ab9d60824b1c9b067489a255b4c7b3111f1a
Huang xuefeng <timhxf@gmail.com>
2014-12-23 22:14:19 +0800

    changed GUI component

--- ffunction.cpp
+++ ffunction.cpp
@@ -1,23 +1,48 @@
 #include "ffunction.h"
 #include "ui_F_Function.h"
-#include <QtWebKitWidgets/QWebView>
-#include <QUrl>
+//#include <QtWebKitWidgets/QWebView>
+//#include <QUrl>
 
 FFunction::FFunction(QWidget *parent)
   :QWizardPage(parent), ui(new Ui::F_Function)
 {
   ui -> setupUi(this);
-  ui -> webView -> load(QUrl("http://en.wikipedia.org/wiki/Data_Encryption_Standard"));
+//  ui -> webView -> load(QUrl("http://en.wikipedia.org/wiki/Data_Encryption_Standard"));
 
-  ui -> title -> setText("<h1>3. F function</h1>");
-  ui -> description -> setText("F function is the most important part in "
-                               "DES, <b>SBox</b> provides confusion and "
-                               "<b>Permutation box</b> provides diffusion, "
-                               "choose the one you want to enable:");
+  ui -> title -> setText("<h1>3. Select cipher component</h1>");
+  ui -> description -> setText("Select components used in cipher:");
   ui -> description -> setWordWrap(true);
+  //set number of rounds
+  for(int i = 0; i <= 16; i++){
+      ui -> num_rounds -> addItem(QString::number(i));
+    }
+  ui -> num_rounds -> setCurrentIndex(0);
 
-  registerField("check_sbox", ui -> check_sbox);
+  //set sbox value
+  for(int i = 1; i <= 8; i++){
+      ui -> S1 -> addItem("S" + QString::number(i));
+      ui -> S2 -> addItem("S" + QString::number(i));
+      ui -> S3 -> addItem("S" + QString::number(i));
+      ui -> S4 -> addItem("S" + QString::number(i));
+      ui -> S5 -> addItem("S" + QString::number(i));
+      ui -> S6 -> addItem("S" + QString::number(i));
+      ui -> S7 -> addItem("S" + QString::number(i));
+      ui -> S8 -> addItem("S" + QString::number(i));
+    }
+
+  registerField("check_swap", ui -> check_swap);
+  registerField("check_IP", ui -> check_IP);
+  registerField("check_inverse_IP", ui -> check_inverse_IP);
   registerField("check_permutation", ui -> check_permutarion);
+  registerField("num_rounds", ui -> num_rounds);
+  registerField("S1", ui -> S1);
+  registerField("S2", ui -> S2);
+  registerField("S3", ui -> S3);
+  registerField("S4", ui -> S4);
+  registerField("S5", ui -> S5);
+  registerField("S6", ui -> S6);
+  registerField("S7", ui -> S7);
+  registerField("S8", ui -> S8);
 }
 
 FFunction::~FFunction(){
